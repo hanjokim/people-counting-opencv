cb313d0a598687848c95953d4ed70bd121f9f350
hanjokim <hanjokim@users.noreply.github.com>
2021-04-13 13:58:37 +0900

    Update video_capture.py

--- video_capture.py
+++ video_capture.py
@@ -16,7 +16,7 @@ h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS) # 카메라에 따라 값이 정상적, 비정상적
 
 # fourcc 값 받아오기, *는 문자를 풀어쓰는 방식, *'DIVX' == 'D', 'I', 'V', 'X'
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 
 # 1프레임과 다음 프레임 사이의 간격 설정
 delay = round(1000/fps)
